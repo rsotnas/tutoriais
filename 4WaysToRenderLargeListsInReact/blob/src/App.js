cfcf78647727d3c914ce0fe99e3696fe31ee10e2
Felipe <fas.felipesantos@gmail.com>
2020-11-24 10:06:02 -0300

    x

---
+++ 4WaysToRenderLargeListsInReact/blob/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import faker from 'faker';
+import './App.css';
+
+const data = new Array(10000).fill().map((value, index) => ({id: index, title: faker.lorem.sentences(4)}))
+
+function App() {
+  return (
+    <div>
+      {
+        data.map((item) => (
+          <div key={item.id} className="post"> 
+            <h3>{item.title} - {item.id}</h3>
+            <p>{item.body}</p>
+          </div>
+        ))
+      }
+    </div>
+  );
+}
+
+export default App;
